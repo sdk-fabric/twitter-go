034f5770f3b35f298e0590f186548fc4ab49a296
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-01-26 22:02:42 +0100

    initial commit

---
+++ client_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/apioo/twitter-go/sdk"
+	"github.com/apioo/sdkgen-go"
+	"reflect"
+	"testing"
+)
+
+func TestClient(t *testing.T) {
+
+	var credentials = sdkgen.HttpBearer{
+		Token: "",
+	}
+
+	client, err := sdk.NewClient("https://api.acme.com", credentials)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sdk-fabric/twitter-go
+
+go 1.18
+
+require github.com/apioo/sdkgen-go v1.0.2
